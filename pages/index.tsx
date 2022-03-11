eca54b22e244b4d7c504869b2e82207014a79a61
Neeraj Dalal <admin@nrjdalal.com>
2022-03-12 00:07:30 +0530

    Sat Mar 12 00:07:30 IST 2022

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,29 +2,31 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-center p-5">
-      {
-        // ~ store
-      }
-      <label>Store Name</label>
-      <input type="text" />
-      {
-        // ~ name
-      }
-      <label>Full Name</label>
-      <input type="text" />
-      {
-        // ~ email
-      }
-      <label>Email</label>
-      <input type="text" />
-      {
-        // ~ password
-      }
-      <label>Password</label>
-      <input type="password" />
+    <div className="flex justify-center">
+      <div className="flex h-screen w-full max-w-screen-sm flex-col items-center justify-center p-5">
+        {
+          // ~ store
+        }
+        <label>Store Name</label>
+        <input type="text" />
+        {
+          // ~ name
+        }
+        <label>Full Name</label>
+        <input type="text" />
+        {
+          // ~ email
+        }
+        <label>Email</label>
+        <input type="text" />
+        {
+          // ~ password
+        }
+        <label>Password</label>
+        <input type="password" />
 
-      <button className="mt-10 h-8 w-full rounded-md bg-emerald-500 text-white">Submit</button>
+        <button className="mt-10 h-10 w-full rounded-md bg-emerald-500 text-white">Submit</button>
+      </div>
     </div>
   )
 }
