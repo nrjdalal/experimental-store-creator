6c9ebec078a333d265398ec4f014409f831b9e48
Neeraj Dalal <admin@nrjdalal.com>
2022-03-11 22:34:31 +0530

    Fri Mar 11 22:34:31 IST 2022

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center p-5">
+      {
+        // ~ store
+      }
+      <label>Store Name</label>
+      <input type="text" />
+      {
+        // ~ name
+      }
+      <label>Full Name</label>
+      <input type="text" />
+      {
+        // ~ email
+      }
+      <label>Email</label>
+      <input type="text" />
+      {
+        // ~ password
+      }
+      <label>Password</label>
+      <input type="password" />
+      {
+        // ~ country
+      }
+      <label>Store Country</label>
+      <select>
+        <option value="India">India</option>
+        <option value="USA">USA</option>
+      </select>
+
+      <button className="mt-10 h-8 w-full rounded-md bg-blue-500 text-white">Submit</button>
+    </div>
+  )
 }
+
+export default Home
